c68867d677a7ad077a26d95cd1264e2caa867b0d
Shivang2910 <shivangbhatnagar2910@gmail.com>
2019-07-15 15:07:01 +0530

    11

--- App_Code/Bill.cs
+++ App_Code/Bill.cs
@@ -424,8 +424,8 @@ public class Bill
         try
         {
             DataAccess dacess = new DataAccess();
-            String Generatebill = "Insert into " + TableName + "(FlatID,SocietyBillID,BillStartDate,BillEndDate,CurrentBillAmount,CycleType,PaymentDueDate,BillMonth,PreviousMonthBalance,ModifiedAt,BillDescription, SocietyID) Values('"
-                + newBill.FlatID + "','" + newBill.SocietyBillID + "','" + DateString(newBill.BillStartDate ,true) + "','" + DateString(newBill.BillEndDate, true) + "','" + newBill.CurrentBillAmount + "','" + newBill.CycleType + "','" + DateString(newBill.PaymentDueDate ,false) + "','" + DateString(newBill.BillMonth ,false) + "','" + newBill.PreviousMonthBalance + "','" + DateString(newBill.ModifiedAt ,true) + "','" + newBill.BillDescription +"'," + SessionVariables.SocietyID + ")";
+            String Generatebill = "Insert into " + TableName + "(FlatID,SocietyBillID,BillStartDate,BillEndDate,CurrentBillAmount,CycleType,PaymentDueDate,BillMonth,PreviousMonthBalance,ModifiedAt,BillDescription, SocietyID, AmountPaidDate, ActionType) Values('"
+                + newBill.FlatID + "','" + newBill.SocietyBillID + "','" + DateString(newBill.BillStartDate ,true) + "','" + DateString(newBill.BillEndDate, true) + "','" + newBill.CurrentBillAmount + "','" + newBill.CycleType + "','" + DateString(newBill.PaymentDueDate ,false) + "','" + DateString(newBill.BillMonth ,false) + "','" + newBill.PreviousMonthBalance + "','" + DateString(newBill.ModifiedAt ,true) + "','" + newBill.BillDescription +"'," + SessionVariables.SocietyID + "," + DateString(newBill.AmountPaidDate, true) + "," + newBill.ActionType + "')";
             return dacess.Update(Generatebill);
         }
         catch(Exception ex)
